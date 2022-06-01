674ca84043736884cce881cc7cafca81d0c220a5
Marco Kaufmann <marcokaufmann@malako.xyz>
2022-06-01 22:34:28 +0200

    Support client side routing

--- src/main/java/ch/fhnw/palletpals/controller/IndexController.java
+++ src/main/java/ch/fhnw/palletpals/controller/IndexController.java
@@ -7,13 +7,11 @@ package ch.fhnw.palletpals.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping(path = "/")
 public class IndexController {
 
-    @GetMapping
+    @GetMapping(path = "/")
     public String getIndexView(){
         return "index.html";
     }
